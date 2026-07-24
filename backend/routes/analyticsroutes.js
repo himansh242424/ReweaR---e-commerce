51918f01fbaed1fbe91d631cd9fc5d046b7619ed
himansh242424 <juniorpandit20022002@gmail.com>
2026-07-24 16:09:15 +0530

    Add root gitignore and update product routes

--- backend/routes/analyticsroutes.js
+++ backend/routes/analyticsroutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
 const { admin } = require("../middleware/adminMiddleware");
-const { getAdminStats } = require("../controller/analyticsController");
+const { getAdminStats } = require("../controller/analyticscontroller");
 
 const router = express.Router();
 router.get("/", protect, admin, getAdminStats);
